92adb28da933f919707b7563c35f973f24d3bf97
wlj <wlj@prodeMacBook-Pro.local>
2019-06-19 14:23:29 +0800

    wang

--- src/api/jk.js
+++ src/api/jk.js
@@ -4,7 +4,7 @@ export default {
   'logout': 'api/logout', // 登出
   'channellogin': 'api/login/channellogin', // 渠道登陆
   // 欢迎页
-  'selectQu': 'api/chart/selectQu', // 渠道统计-饼图
+  'selectQu': 'api/chart/selectQus', // 渠道统计-饼图
   'selectLoanRepayment': 'api/chart/selectLoanRepayment', // 放/还款统计-折线
   'selectTotalLogByTime': 'api/order/selectTotalLogByTime', // 查询
   // 会员管理
